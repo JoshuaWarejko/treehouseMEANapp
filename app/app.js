c531b4384a119d2728613b5ed4babea72c3b6b10
Joshua Warejko <josh@newleaf.team>
2016-07-21 23:33:27 -0700

    Final edits. App works as it should via Treehouse course

--- app/app.js
+++ app/app.js
@@ -5,3 +5,6 @@ var angular = require('angular');
 angular.module('todoListApp', []);
 
 require('./scripts/controllers/main.js');
+require('./scripts/controllers/todo.js');
+require('./scripts/services/data.js');
+require('./scripts/directives/todo.js');
--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 'use strict';
 
+debugger;
+
 var express = require('express');
+var parser = require('body-parser');
 var router = require('./api');
 
 var app = express();
 
+require('./database');
+require('./seed');
+
 app.use('/', express.static('public'));
+app.use(parser.json());
 
 app.use('/api', router);
 
